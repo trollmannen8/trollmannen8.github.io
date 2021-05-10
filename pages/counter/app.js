0c9549f098807ec07641fd0fe917518f686758bd
Dániel Takács <80113055+trollmannen8@users.noreply.github.com>
2021-05-10 19:38:59 +0200

    Plus One Subpage

---
+++ pages/counter/app.js
@@ -0,0 +1,27 @@
+var count = 0;
+
+var value = document.querySelector("#value");
+var btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(btn) {
+    btn.addEventListener("click", function(e) {
+        let styles = e.currentTarget.classList;
+        if (styles.contains("decrease")) {
+            count --;
+        } else if (styles.contains("increase")) {
+            count ++;
+        } else {
+            count = 0;
+        }
+        if (count > 0) {
+            value.style.color = "green";
+        }
+        if (count < 0) {
+            value.style.color = "red";
+        }
+        if (count == 0) {
+            value.style.color = "black";
+        }
+        value.textContent = count;
+    });
+});
