4456f47d905eacc0963242ff2584333265537b92
Dániel Takács <80113055+trollmannen8@users.noreply.github.com>
2021-06-01 21:25:55 +0200

    Update

--- pages/javascript/digitalclock/digitalclock.js
+++ pages/javascript/digitalclock/digitalclock.js
@@ -1,12 +1,27 @@
 var hours = document.getElementById("hours");
 var minutes = document.getElementById("minutes");
 var seconds = document.getElementById("seconds");
+var weekday = document.getElementById("weekday");
+var month = document.getElementById("month");
+var day = document.getElementById("day");
+var week = {
+    0: "SU",
+    1: "MO",
+    2: "TU",
+    3: "WE",
+    4: "TH",
+    5: "FR",
+    6: "SA"
+}
 
 function displayTime() {
     var time = new Date();
     var hoursValue = time.getHours();
     var minutesValue = time.getMinutes();
     var secondsValue = time.getSeconds();
+    var weekdayValue = week[time.getDay()];
+    var monthValue = time.getMonth() + 1;
+    var dayValue = time.getDate();
     if (hoursValue < 10) {
         hoursValue = "0" + hoursValue.toString();
     }
@@ -17,6 +32,9 @@ function displayTime() {
         secondsValue = "0" + secondsValue.toString();
     }
     
+    weekday.innerHTML = weekdayValue;
+    month.innerHTML = monthValue;
+    day.innerHTML = dayValue
     hours.innerHTML = hoursValue;
     minutes.innerHTML = minutesValue;
     seconds.innerHTML = secondsValue;
