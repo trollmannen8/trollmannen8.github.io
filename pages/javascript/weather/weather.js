476deca212d1030b154c7031ce9346c30d66fe8f
Dániel Takács <80113055+trollmannen8@users.noreply.github.com>
2021-06-03 20:04:03 +0200

    Developing

--- pages/javascript/weather/weather.js
+++ pages/javascript/weather/weather.js
@@ -1,12 +1,11 @@
 const form = document.querySelector(".top-banner form");
 const input = document.querySelector(".top-banner input");
 const msg = document.querySelector(".top-banner .msg");
-const list = document.querySelector(".ajax-section .cities");
+const content = document.querySelector("#content");
 const apiKey = "944cb94e82e619709c4178e16db05e96";
 
 form.addEventListener("submit", e => {
   e.preventDefault();
-  const listItems = list.querySelectorAll(".ajax-section .city");
   const inputVal = input.value;
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
@@ -17,23 +16,25 @@ form.addEventListener("submit", e => {
       const { main, name, sys, weather } = data;
       const icon = `https://openweathermap.org/img/wn/${
         weather[0]["icon"]
-      }@2x.png`;
+      }@4x.png`;
 
-      const li = document.createElement("li");
-      li.classList.add("city");
       const markup = `
         <h2 class="city-name" data-name="${name},${sys.country}">
           <span>${name},</span>
           <span>${sys.country}</span>
         </h2>
-        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
-        <figure>
-          <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
-          <figcaption>${weather[0]["description"]}</figcaption>
-        </figure>
+        <div class="display">
+          <figure>
+            <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
+          </figure>
+          <div class="weather-text">
+            <div class="city-temp">${Math.round(main.temp)} °C</div>
+            <div class="description-text">${weather[0]["description"]}</div>
+          </div>
+        </div>
       `;
-      li.innerHTML = markup;
-      list.appendChild(li);
+      content.innerHTML = markup;
+      document.querySelector(".ajax-section").style.display = "block";
     })
     .catch(() => {
       msg.textContent = "This is not a valid city.";
