f3f3cc8cd7bd55484039b22170cfb517b1d997d0
trollmannen8 <80113055+trollmannen8@users.noreply.github.com>
2021-06-03 15:45:13 +0200

    Getting Started

---
+++ pages/javascript/weather/weather.js
@@ -0,0 +1,45 @@
+const form = document.querySelector(".top-banner form");
+const input = document.querySelector(".top-banner input");
+const msg = document.querySelector(".top-banner .msg");
+const list = document.querySelector(".ajax-section .cities");
+const apiKey = "944cb94e82e619709c4178e16db05e96";
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const listItems = list.querySelectorAll(".ajax-section .city");
+  const inputVal = input.value;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const { main, name, sys, weather } = data;
+      const icon = `https://openweathermap.org/img/wn/${
+        weather[0]["icon"]
+      }@2x.png`;
+
+      const li = document.createElement("li");
+      li.classList.add("city");
+      const markup = `
+        <h2 class="city-name" data-name="${name},${sys.country}">
+          <span>${name},</span>
+          <span>${sys.country}</span>
+        </h2>
+        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+        <figure>
+          <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
+          <figcaption>${weather[0]["description"]}</figcaption>
+        </figure>
+      `;
+      li.innerHTML = markup;
+      list.appendChild(li);
+    })
+    .catch(() => {
+      msg.textContent = "This is not a valid city.";
+    });
+
+  msg.textContent = "";
+  form.reset();
+  input.focus();
+});
