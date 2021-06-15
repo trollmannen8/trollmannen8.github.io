2d4a19704a4e191b167924b5bd23aed8edde2890
trollmannen8 <80113055+trollmannen8@users.noreply.github.com>
2021-06-15 16:08:35 +0200

    Update bgp.js

--- pages/javascript/BreakOutGamePhaser/bgp.js
+++ pages/javascript/BreakOutGamePhaser/bgp.js
@@ -5,18 +5,25 @@ var game = new Phaser.Game(480, 320, Phaser.CANVAS, null, {
 var ball;
 
 function preload() {
+    handleRemoteImagesOnJSFiddle();
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     game.scale.pageAlignHorizontally = true;
     game.scale.pageAlignVertically = true;
     game.stage.backgroundColor = "#eee";
-    game.load.image("ball", "img/ball.png");
+    game.load.image('ball', 'img/ball.png');
 };
 
 function create() {
-    ball = game.add.sprite(50, 50, "ball");
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    ball = game.add.sprite(50, 50, 'ball');
+    game.physics.enable(ball, Phaser.Physics.ARCADE);
+    ball.body.velocity.set(150, 150);
 };
 
-function update() {
-    ball.x += 1;
-    ball.y += 1;
-};
+function update() {};
+
+// this function (needed only on JSFiddle) take care of loading the images from the remote server
+function handleRemoteImagesOnJSFiddle() {
+	game.load.baseURL = 'https://end3r.github.io/Gamedev-Phaser-Content-Kit/demos/';
+	game.load.crossOrigin = 'anonymous';
+}
