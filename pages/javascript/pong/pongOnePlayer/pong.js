eda3468ad612650e9170f6bc2f0742d564ab8806
trollmannen8 <80113055+trollmannen8@users.noreply.github.com>
2021-06-30 12:10:38 +0200

    Update pong.js

--- pages/javascript/pong/pongOnePlayer/pong.js
+++ pages/javascript/pong/pongOnePlayer/pong.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d", { alpha: false });
 const netWidth = 4;
 const netHeight = canvas.height;
 const paddleWidth = 10;
